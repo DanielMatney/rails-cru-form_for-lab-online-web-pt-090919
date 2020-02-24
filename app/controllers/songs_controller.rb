7399271f7833ec3f27422e5066696f7b3d6a23d2
Daniel Matney <danieljmatney@gmail.com>
2020-02-24 06:07:18 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,33 @@
 class SongsController < ApplicationController
+  
+  def new
+		@song = Song.new
+	end
+  
+  def create
+    @song = Artist.new(artist_params(:name, :bio))
+	  @song.save
+	  redirect_to artist_path(@song)
+  end
+
+  def update
+    @song = Artist.find(params[:id])
+	  @song.update(artist_params(:name, :bio))
+	  redirect_to artist_path(@song)
+  end
+
+  def show
+    @song = Artist.find(params[:id])
+  end
+  
+  def edit
+	  @song = Artist.find(params[:id])
+	end
+	
+  private
+	
+	def song_params(*args)
+  params.require(:song).permit(*args)
+  end
+  
 end
