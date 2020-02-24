f368077dec6cf908528d02f6094289f85f520ec1
Daniel Matney <danieljmatney@gmail.com>
2020-02-24 06:00:09 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,4 +14,10 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+  
+  private
+	
+	def artist_params(*args)
+  params.require(:artist).permit(*args)
+  end
 end
